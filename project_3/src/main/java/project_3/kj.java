7f2ab3d873fd3774d2a6bf9d3408054d6c151cb1
mirindra rabenjarijaona <hery.rabenjarijaona@epita.fr>
2022-04-05 11:35:43 +0200

    projet

---
+++ project_3/src/main/java/project_3/kj.java
@@ -0,0 +1,17 @@
+package project_3;
+
+public class kj {
+
+	public kj() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello wolrd");
+
+	}
+	
+
+
+}
---
+++ project_3/src/main/java/project_3/piece.java
@@ -0,0 +1,13 @@
+package project_3;
+
+public class piece {
+
+	public piece() {
+		int id;
+		String position;
+		type type;
+		equipe equipe;
+		
+	}
+
+}
