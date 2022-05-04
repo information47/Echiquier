f56a7780b1fab444c6ac6165d4159e16ff2c040f
mirindra rabenjarijaona <hery.rabenjarijaona@epita.fr>
2022-05-04 16:11:53 +0200

    commentaire sur knight

--- src/fr/intech/echecs/model/pieces/Knight.java
+++ src/fr/intech/echecs/model/pieces/Knight.java
@@ -11,7 +11,8 @@ import fr.intech.echecs.model.chessboard.Move;
 public class Knight extends Pieces {
 	
 	private Type type;
-	private static int[][] possible_move_xy = {{-1, 2}, {1,2}, {2,-1}, {2,1},{-2,1}, {-2,-1}, {-1,-2}, {1,-2}};
+	private static int[][] possible_move_xy = {{-1, 2}, {1,2}, {2,-1}, {2,1},{-2,1}, {-2,-1}, {-1,-2}, {1,-2}}; // liste des opérations à faire sur les coordonnée du 
+	//chevalier pour connaitre les cases atteignables
 	
 
 	public Knight(int x, int y, Team team) {
@@ -26,44 +27,38 @@ public class Knight extends Pieces {
 
 	@Override
 	public List<Move> legal_move(Board board) {
-		List<Move> FinalList = new ArrayList<Move>();
+		List<Move> FinalList = new ArrayList<Move>();  // la liste de tout les mouvements possible
 		for (int[] coordinate : possible_move_xy) {
 			
 			int new_x;
 			int new_y;
 			List<Move> LegalMove = new ArrayList<Move>();
 			
-			new_x = coordinate[0] + this.x;
+			new_x = coordinate[0] + this.x;  
 			new_y = coordinate[1] + this.y;
 			
-			int[] possible_destination = {new_x, new_y};
+			int[] possible_destination = {new_x, new_y}; // on identifie les coordonnées atteignablent
 			
-			if ( CellExist(possible_destination)) {
+			if ( CellExist(possible_destination)) {       // est ce que la case existe
 				
 				Cell corresponding_cell = Board.GetCell(possible_destination);
 				
-				if(!corresponding_cell.IsEmpty()) {
+				if(!corresponding_cell.IsEmpty()) { // est ce qu'elle est vide si ou on l'ajoute au mouvements possibles
 					LegalMove.add(new Move());
 				}
 				else {
-					Pieces PieceOnCell = corresponding_cell.GetPiece();
-					Team PieceTeam = PieceOnCell.GetTeam();
+					Pieces PieceOnCell = corresponding_cell.GetPiece();  //quel pièce est dessus
+					Team PieceTeam = PieceOnCell.GetTeam();     // est elle amie ou ennemie
 					
-					if (this.team != PieceTeam) {
+					if (this.team != PieceTeam) {   //  si ennemie on l'ajoute au mouvements possibles
 						LegalMove.add(new Move());
-					}
-					
-				}
-				
-				
+					}	
+				}		
 			}
 			
-			FinalList = LegalMove;
-		}
-		
-		
-			
-		return FinalList;
+			FinalList = LegalMove; // finalList prend la valeur de tout les mouvements possibles
+		}	
+		return FinalList; // on retourne la liste de tout les mouvements possible
 	}
 
 	
