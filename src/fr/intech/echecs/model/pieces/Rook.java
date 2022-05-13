9be6981044f6dd9c2715b773305df419f4565537
NYAMSI10 <bnyamsi10@gmail.com>
2022-05-13 10:26:55 +0200

    new

---
+++ src/fr/intech/echecs/model/pieces/Rook.java
@@ -0,0 +1,29 @@
+package fr.intech.echecs.model.pieces;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.intech.echecs.model.chessboard.Board;
+import fr.intech.echecs.model.chessboard.Move;
+
+
+
+public class Rook  extends Pieces{
+
+	  private Type type;
+	
+	public Rook(int x, int y, Team team) {
+		super(x, y, team);
+		this.type = Type.ROOK;
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public List<Move> legal_move(Board board) {
+		List<Move> FinalList = new ArrayList<Move>();  // la liste de tout les mouvements possible
+
+		return null;
+	}
+ 
+
+}
