e6693adf47753f74a7d2e89f75bffc8ff53f3ff8
DEVRED Paul <devred@et.intechinfo.fr>
2022-06-30 10:35:27 +0200

    ajout des backgrounds sans URL

--- src/fr/intech/echecs/MainEchec.java
+++ src/fr/intech/echecs/MainEchec.java
@@ -8,6 +8,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
